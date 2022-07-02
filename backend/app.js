65b43a150b1245317ecc7df4e77728d0788ba148
Diana <kozachenkodiana03@mail.ru>
2022-07-02 12:27:20 +0700

    [TM-3] Подключение всех основных пакетов

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const morgan = require('morgan');
+
+const CONFIG = require('./config/index')
+const app = express()
+
+app.use(morgan(CONFIG.morganFormant))
+
+module.exports = app
