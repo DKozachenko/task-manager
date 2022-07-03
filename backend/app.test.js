844926a3094d8cc1807b98e62271aecf07cfe0f1
Diana <kozachenkodiana03@mail.ru>
2022-07-03 18:10:43 +0700

    [TM-26] Завершение тестов

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 const supertest = require('supertest');
 const app = require('./app');
-
 const request = supertest(app);
 
 describe('/test endpoint', () => {
@@ -9,6 +8,5 @@ describe('/test endpoint', () => {
     const response = await request.get('/test');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Passed');
-    done();
   });
 });
