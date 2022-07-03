2a6ccf8c52da67f076b68ac4f42bed6f8277c54b
Diana <kozachenkodiana03@mail.ru>
2022-07-03 17:28:24 +0700

    [TM-26] Добавлен логер для консоли

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const CONFIG = require('./config/index');
+
+const logger = require('./logger');
+
 const app = express();
 
 /** Remove test method */
@@ -12,8 +15,8 @@ app.get('/test', (req, res) =>  {
 
 /** Connection to MongoDb */
 mongoose.connect(CONFIG.mongoUrl)
-  .then(() => console.log('Connected to MongoDb'))
-  .catch(error => console.error(error));
+  .then(() => logger.info('Connected to MongoDb'))
+  .catch(error => logger.error('Connection to MongoDb failed', error));
 
 app.use(morgan(CONFIG.morganFormat));
 
---
+++ backend/logger.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+/* eslint-disable array-bracket-newline */
+const winston = require('winston');
+
+const colorsAndTime = winston.format.combine(
+  winston.format.colorize({
+    all:true
+  }),
+  winston.format.label({
+    label:'[LOGGER]'
+  }),
+  winston.format.timestamp({
+    format: 'DD-MM-YY HH:mm:ss'
+  }),
+  winston.format.printf(
+    info => `${info.label} ${info.timestamp} ${info.message}`
+  )
+);
+    
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new (winston.transports.Console)({
+      format: winston.format.combine(winston.format.colorize(), colorsAndTime)
+    })
+  ],
+});
+
+module.exports = logger;
