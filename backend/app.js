23976ca83c7ae11f80ec178e51588304482f3833
Diana <kozachenkodiana03@mail.ru>
2022-07-03 14:31:36 +0700

    [TM-4] Добавлен тест для запуска сервера

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,11 @@ const morgan = require('morgan');
 const CONFIG = require('./config/index');
 const app = express();
 
+/** Remove test method */
+app.get('/test', (req, res) =>  {
+  res.status(200).send('Passed');
+});
+
 app.use(morgan(CONFIG.morganFormat));
 
 module.exports = app;
---
+++ backend/app.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+const supertest = require('supertest');
+const app = require('./app');
+
+const request = supertest(app);
+
+describe('/test endpoint', () => {
+  it('should return a response', async () => {
+    const response = await request.get('/test');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Passed');
+  });
+});
