6d512e95c381b4ac1362f7b4758ce2ac468d3872
Diana <kozachenkodiana03@mail.ru>
2022-07-24 15:45:48 +0700

    [TM-27] Добавлено логирование в файлы

--- backend/src/logger/logger.js
+++ backend/src/logger/logger.js
@@ -2,10 +2,22 @@
 /* eslint-disable array-bracket-newline */
 const winston = require('winston');
 
-const colorsAndTime = winston.format.combine(
+const colorsAndTimeFormat = winston.format.combine(
   winston.format.colorize({
     all:true
   }),
+  winston.format.label({
+    label:'[LOGGER]'
+  }),
+  winston.format.timestamp({
+    format: 'DD-MM-YY HH:mm:ss'
+  }),
+  winston.format.printf(
+    info => `${info.label} ${info.timestamp} ${info.message}`
+  )
+);
+
+const fileFormat = winston.format.combine(
   winston.format.label({
     label:'[LOGGER]'
   }),
@@ -27,7 +39,22 @@ const logger = winston.createLogger({
   level: 'info',
   transports: [
     new (winston.transports.Console)({
-      format: winston.format.combine(winston.format.colorize(), colorsAndTime)
+      format: winston.format.combine(winston.format.colorize(), colorsAndTimeFormat)
+    }),
+    new winston.transports.File({ 
+      filename: 'logs/error.log',
+      level: 'error',
+      format: fileFormat
+    }),
+    new winston.transports.File({ 
+      filename: 'logs/warn.log',
+      level: 'warn',
+      format: fileFormat
+    }),
+    new winston.transports.File({ 
+      filename: 'logs/info.log',
+      level: 'info',
+      format: fileFormat
     })
   ],
 });
